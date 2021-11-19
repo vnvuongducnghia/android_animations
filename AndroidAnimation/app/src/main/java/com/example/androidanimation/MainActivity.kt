165020c9e9e7d072bcd75051ecbc2958fa2654c4
vuongducnghia <vnvuongducnghia@gmail.com>
2021-11-19 09:36:38 +0700

    no message

--- AndroidAnimation/app/src/main/java/com/example/androidanimation/MainActivity.kt
+++ AndroidAnimation/app/src/main/java/com/example/androidanimation/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.example.androidanimation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
+import android.graphics.Path
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Move 1
+        val path = Path()
+        path.moveTo(500f, 500f)
+        path.lineTo(500f, 100f)
+        path.setLastPoint(0f, 0f)
+        path.close()
+        val pathAnimator = ObjectAnimator.ofFloat(imgAM, "x", "y", path)
+
+
+        // Move 2
+        val animMove = AnimationUtils.loadAnimation(applicationContext, R.anim.scm_capture)
+
+
+
+        btnMove.setOnClickListener {
+//            imgAM.startAnimation(animMove)
+            pathAnimator.duration = 3000
+            pathAnimator.start()
+        }
     }
 }
+
+internal class Pt(var x: Float, var y: Float)
